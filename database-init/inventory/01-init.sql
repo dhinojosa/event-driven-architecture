6656421f7cbe7f488a98bc0074f2679e1ae7d447
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2025-12-02 12:33:09 -0700

    Applied an outbox to the inventory so that we can have state and send messages out. Also migrated to the maven plugin

--- database-init/inventory/01-init.sql
+++ database-init/inventory/01-init.sql
@@ -11,18 +11,10 @@ CREATE TABLE product
 CREATE INDEX idx_product_name ON product (name);
 
 
-CREATE TABLE product_outbox
-(
-    id BIGSERIAL PRIMARY KEY,
-    aggregate_type VARCHAR(50) NOT NULL,
-    aggregate_id UUID NOT NULL,
-    event_type     VARCHAR(50) NOT NULL,
-    payload JSONB NOT NULL,
-    timestamp      TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    FOREIGN KEY (aggregate_id)
-        REFERENCES product (productID)
+CREATE TABLE product_outbox (
+                                id UUID PRIMARY KEY,
+                                aggregatetype VARCHAR(255) NOT NULL,
+                                aggregateid VARCHAR(255) NOT NULL,
+                                type VARCHAR(255) NOT NULL,
+                                payload BYTEA
 );
-
-CREATE INDEX idx_product_outbox_ts ON product_outbox (timestamp);
-CREATE INDEX idx_product_outbox_aggregate_id ON product_outbox (aggregate_id);
